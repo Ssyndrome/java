a1a279cf5e4f3e49358dd085334823f3fe9154e2
Ssyndrome <915448715@qq.com>
2018-09-09 18:39:29 +0800

    add StringReverser

--- src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
@@ -1,13 +1,25 @@
 package com.cultivation.javaBasicExtended.reverseString;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.Arrays;
 
 class StringReverser {
     @SuppressWarnings({"WeakerAccess", "unused"})
     public static String[] reverse(String input) {
         // TODO: please implement the method to pass all the tests.
         // <--start
-        throw new NotImplementedException();
+
+        if (input == null) throw new IllegalArgumentException();
+
+        String[] originalStringArray = input.equals("") ? new String[0] : input.split(" ");
+
+        int length = originalStringArray.length;
+        String[] resultArray = new String[length];
+
+        for (int originalIndex = 0, resultIndex = length-1; originalIndex < length; originalIndex++) {
+            resultArray[resultIndex] = originalStringArray[originalIndex];
+            resultIndex --;
+        }
+        return resultArray;
         // --end-->
     }
 }
